1bd81dad385159964d7c48c2593ad00e2ef3d1a1
arnavgaur04 <arnavgaur04@gmail.com>
2024-03-20 06:10:21 +0530

    fixing some issue

--- bynry/members/views.py
+++ bynry/members/views.py
@@ -16,7 +16,9 @@ def newrequest(request):
     print("this is post")
     requestType = request.POST['type']
     text = request.POST['text']
-    imagePath = request.FILES['fileName']
+    imagePath = None
+    if request.FILES:
+      imagePath = request.FILES['fileName']
     print(imagePath)
     Request_instance = Request.objects.create(requestBy = requestBy, requestType = requestType, requestDetails=text, requestImage = imagePath)
 
